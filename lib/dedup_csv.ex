0961f1fdf2aa7ba797c9e95d1397e7b347e723c0
David Spigarelli <david.spigarelli@expel.io>
2021-08-03 17:54:40 -0400

    Initial commit

---
+++ lib/dedup_csv.ex
@@ -0,0 +1,18 @@
+defmodule DedupCsv do
+  @moduledoc """
+  Documentation for `DedupCsv`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DedupCsv.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dedup_csv_test.exs
@@ -0,0 +1,8 @@
+defmodule DedupCsvTest do
+  use ExUnit.Case
+  doctest DedupCsv
+
+  test "greets the world" do
+    assert DedupCsv.hello() == :world
+  end
+end
